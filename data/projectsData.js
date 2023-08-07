70851ab39b1941c594dc082aae005ccfb458e1ff
Riyan Ahmed <riyan.ahmed@outlook.com>
2023-08-07 04:15:02 -0400

    first commit

---
+++ data/projectsData.js
@@ -0,0 +1,16 @@
+const projectsData = [
+  {
+    title: 'Fabrilab',
+    description: `Creation of a web design agency.`,
+    imgSrc: '/static/images/projects/fabrilab.png',
+    href: '/blog/projects/fabrilab-web-agency',
+  },
+  {
+    title: 'Toy Surfboards',
+    description: `Creation & developpement of a e-commerce website.`,
+    imgSrc: '/static/images/projects/browser_toy.png',
+    href: '/blog/projects/toy-surfboards',
+  },
+];
+
+export default projectsData;
